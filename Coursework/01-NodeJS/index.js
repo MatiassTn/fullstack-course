dcf82e4d26c4a2193f52a449c3d2fa46c9791a2c
MatiassTn <Matias.Tanskanen@student.lut.fi>
2026-08-22 12:58:53 +0300

    Nodejs video / saving

---
+++ Coursework/01-NodeJS/index.js
@@ -0,0 +1,12 @@
+import getPosts, { getPostsLength } from './postController.js';
+
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostsLength()}`);
+
+
+//const {generateRandomNumber, celsiusToFarenheit} = require('./utils');
+
+//console.log(`Random number: ${generateRandomNumber()}`);
+
+//console.log(`Celsius to Farenheit: ${celsiusToFarenheit(0)}`);
